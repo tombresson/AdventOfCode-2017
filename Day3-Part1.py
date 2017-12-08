2de18d3959399c7ff9ca4723531e31834119fb4f
turfyman <turfyman@gmail.com>
2017-12-07 23:16:06 -0500

    Start Day 3

---
+++ Day3-Part1.py
@@ -0,0 +1,36 @@
+
+
+
+def move_right(x,y):
+    return x+1, y
+
+def move_down(x,y):
+    return x,y-1
+
+def move_left(x,y):
+    return x-1,y
+
+def move_up(x,y):
+    return x,y+1
+
+target_value = 368078
+
+spiral_list = []
+
+max_x = 0
+min_x = 0
+max_y = 0
+min_y = 0
+x = 0
+y = 0
+current_value = 1
+# Generate Spiral
+while(current_value < target_value):
+    # Move Right
+
+    # Move Up
+
+    # Move Left
+
+    # Move Down
+
