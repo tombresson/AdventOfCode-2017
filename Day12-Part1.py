c4ba8ddae77be1f769b7f01b33ece6a5c41be605
Tom Bresson <thomas.bresson@mtdproducts.com>
2017-12-13 09:25:20 -0500

    Finish Day12-Part1

--- Day12-Part1.py
+++ Day12-Part1.py
@@ -1,11 +1,18 @@
 from typing import List
 import re
 
+
 class Program(object):
     def __init__(self, id):
         self.id = id
-        self.connected_to_zero = False
-        self.connections = []
+        self.connections= []
+
+def traverse_graph(node: Program, visited_nodes: List[str]):
+    for x in node.connections:
+        x: Program = x
+        if x not in visited_nodes:
+            visited_nodes.append(x)
+            traverse_graph(x, visited_nodes)
 
 def main():
     prgms: List[Program] = []
@@ -31,41 +38,17 @@ def main():
         # this works since the array idx is the same as the read value
         prgm = prgms[int(tokens[0])]
 
-        # Special case 0
-        if prgm.id == '0':
-            prgm.connected_to_zero = True
-
         if len(tokens) > 1:
             # Iterate through the prgms connections
             for token in tokens[1:]:
 
-                if token == '0':
-                    prgm.connected_to_zero = True
-
-                else:
-                    # Check existing node to determine if
-                    # it is connected to zero through another prgm
-                    x: Program = prgms[int(token)]
-                    if x.connected_to_zero:
-                        prgm.connected_to_zero = True
-
                 # Append the connection to the prgm
                 prgm.connections.append(prgms[int(token)])
 
-            # If this node is now connected to zero, ensure all the listed nodes are as well
-            if prgm.connected_to_zero:
-                for x in prgm.connections:
-                    x.connected_to_zero = True
+    nodes_connected_to_zero = []
+    traverse_graph(prgms[0], nodes_connected_to_zero)
 
-
-    connections_cnt = 0
-    for prgm in prgms:
-        if prgm.connected_to_zero:
-            connections_cnt += 1
-
-    print(connections_cnt)
-
-
+    print(len(nodes_connected_to_zero))
 
 
 
